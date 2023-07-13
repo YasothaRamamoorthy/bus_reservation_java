5ea9ff8b56ff7ccfd6e660befb33cabb707fdb51
Yasotha Ramamoorthy <94780018+YasothaRamamoorthy@users.noreply.github.com>
2023-07-13 11:33:11 +0530

    Update BUS.java

--- BUS.java
+++ BUS.java
@@ -1,26 +1,35 @@
 /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package busreservation;
+
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.busreservation;
 
 /**
  *
- * @author yasot
+ * @author yaso
  */
 
-public class BUS {
+public class Bus {
     private int bus_number;
     private Boolean ac; 
     private int capacity;
     
-    BUS(int bs , boolean ac,int cap)
+    Bus(int bs , boolean ac,int cap)
     {
        this.bus_number= bs;
        this.ac=ac;
        this.capacity=cap;
     }
-    
+    public int display_busno()
+    {
+        return this.bus_number;
+    }
     public int display_capacity()
     {
         return this.capacity;
