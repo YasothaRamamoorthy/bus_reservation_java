4ad1d271242de943e8132e211ef21f1156581f0a
Yasotha Ramamoorthy <94780018+YasothaRamamoorthy@users.noreply.github.com>
2023-07-13 08:22:08 +0530

    Create BOOKING.java

---
+++ BOOKING.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.busreservation;
+import java.util.*;
+/**
+ *
+ * @author yasot
+ */
+public class BOOKING {
+    String passanger_name;
+    int bus_no;
+    Date date;
+    
+    BOOKING()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER THE BOO");
+        this.passanger_name=scanner.next();
+        
+    }
+}
